4765e6c4dc94c050ac5beba9aa53a6c6fffb8f09
Pavan Kalyan <pavankalyaninugurthi@gmail.com>
2020-06-18 22:50:37 +0530

    IncrementResource working

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('index',views.index,name='index'),
-    path('<int:question_id>/',views.detail,name='detail'),
-    path('api',views.api_call,name='api')
+	path('index',views.index,name='index'),
+	path('<int:question_id>/',views.detail,name='detail'),
+	path('api',views.api_call,name='api'),
+	path('incOne',views.incOne,name='incOne')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 # from .models import Question
 import requests
 
+p=''
 # Create your views here.
 def index(request):
     data=Question.objects.order_by('-pub_date')[:5]
@@ -16,8 +17,17 @@ def detail(request,question_id):
 
 def api_call(request):
     token = requests.post("https://sport-resources-booking-api.herokuapp.com/login",data={'id': '160118733012','password':'abc123'} )
+    global p
     p = token.json()['access_token']
     data = requests.get("https://sport-resources-booking-api.herokuapp.com/ResourcesPresent", headers = {'Authorization':f'Bearer {p}'}) 
     res = data.json()
     context={'data': res,} 
     return render(request,'api.html',context)
+def incOne(request):
+    id = request.GET['id']
+
+    global p
+    td=requests.get('https://sport-resources-booking-api.herokuapp.com/incrementByValue',headers={'Authorization':f'Bearer {p}'},
+    data={'id':id,'c':1})
+    res = td.json()
+    return JsonResponse(res,safe=False)
