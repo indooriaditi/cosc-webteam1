38bfbbb4109dfa3c8eab40a5a92e1bbdb4886ce3
akshitha.n.123@gmail.com <akshitha.n.123@gmail.com>
2020-06-27 23:30:29 +0530

    added backgrounds

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'sports.apps.SportsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
